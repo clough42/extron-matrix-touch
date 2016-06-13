5f32197e46626bcce8e4385277170cfc5c270412
clough <james.clough@hp.com>
2016-06-13 11:35:48 -0600

    Initial project structure.

---
+++ src/ExtronTouchSwitch/VidSwitch/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtronTouchSwitch
+{
+    class MyClass
+    {
+        private DateTime date;
+       
+        public MyClass()
+        {
+        }
+
+        public int Hours
+        {
+            get { return date.Hour; }
+            set { date = new DateTime(2000, 1, 1, value, 1, 1); }
+        }
+
+
+    }
+}
---
+++ src/ExtronTouchSwitch/VidSwitchTests/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 0);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
